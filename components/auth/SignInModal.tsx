aa3109a09d10eac5653fcf85abc58c88a26f9970
DrPrime01 <drprime.dev@gmail.com>
2026-07-06 14:41:24 +0100

    style: updated signinmodal style

--- components/auth/SignInModal.tsx
+++ components/auth/SignInModal.tsx
@@ -19,24 +19,24 @@ export function SignInModal({ onClose }: { onClose?: () => void }) {
       />
 
       {/* Card */}
-      <div className="relative w-full max-w-70 bg-white rounded-3xl px-6 pt-4 pb-6 flex flex-col items-center text-center gap-3">
-        <div className="absolute top-2 right-2">
+      <div className="relative w-full max-w-70 h-66.25 bg-white rounded-3xl px-6 pt-4 pb-6 flex flex-col items-center text-center">
+        <div className="absolute top-2 right-2.5">
           <button
             onClick={onClose}
-            className="size-10 rounded-full bg-[#E8E6DC] flex items-center justify-center hover:opacity-75 transition-opacity"
+            className="size-7.5 rounded-full bg-grey-c100 flex items-center justify-center hover:opacity-75 transition-opacity"
             aria-label="Close"
           >
-            <X className="size-5 text-foreground" />
+            <X className="size-4.5 text-foreground" />
           </button>
         </div>
 
         <Image src={avatar} alt="image" width={77} height={70} />
 
-        <div className="flex flex-col gap-2 mt-1 items-center">
-          <p className="font-semibold text-3xl text-[#111312] leading-7 lateef-bold">
+        <div className="flex flex-col gap-2 items-center mt-4.25 mb-3.5">
+          <p className="font-semibold text-modaltitle text-black leading-6 lateef-bold">
             Sign in to continue using the app
           </p>
-          <p className="text-xs text-subtle w-10/12">
+          <p className="text-xs text-subtle">
             Please sign in to access expert recipes, favorites and personalised
             guidance.
           </p>
@@ -44,7 +44,7 @@ export function SignInModal({ onClose }: { onClose?: () => void }) {
 
         <button
           onClick={() => router.push("/auth/login")}
-          className="w-full mt-2 py-3 rounded-full bg-mint-green text-white text-sm font-medium hover:opacity-90 transition-opacity"
+          className="w-full py-3 rounded-full bg-mint-green text-white text-sm font-medium hover:opacity-90 transition-opacity"
         >
           Sign in
         </button>
