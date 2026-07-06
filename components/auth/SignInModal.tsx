afb10a6d87a0e915877ccdc34478dde37ded5038
precious654 <odukwechiemeka@gmail.com>
2026-07-06 16:09:42 +0100

    fixed sign in modal image issue

--- components/auth/SignInModal.tsx
+++ components/auth/SignInModal.tsx
@@ -32,7 +32,7 @@ export function SignInModal({ onClose }: { onClose?: () => void }) {
         <SignInModalImage />
 
         <div className="flex flex-col gap-2 items-center mt-4.25 mb-3.5">
-          <p className="font-semibold text-modaltitle text-black leading-6 lateef-bold max-w-49.25">
+          <p className="font-semibold text-modaltitle text-black leading-6 font-lateef max-w-49.25">
             Sign in to continue using the app
           </p>
           <p className="text-xs text-subtle">
