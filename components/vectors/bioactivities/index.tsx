428554452ef2cbd76d9fb570d9328a12fc90df15
precious654 <odukwechiemeka@gmail.com>
2026-07-16 10:00:59 +0100

    updated the bioactivities UI

---
+++ components/vectors/bioactivities/index.tsx
@@ -0,0 +1,7 @@
+import Energy from "./Energy";
+import Stress from "./Stress";
+import Skin from "./Skin";
+import Digestive from "./Digestive";
+import Immunity from "./Immunity";
+
+export { Energy, Stress, Skin, Digestive, Immunity };
